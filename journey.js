952145230cd54b3673749dad7f89941d0c91fb8b
James He <62224382+he-is-james@users.noreply.github.com>
2020-12-25 16:44:56 -0500

    Update journey.js

--- journey.js
+++ journey.js
@@ -8,15 +8,16 @@ let num4_width = 0;
 let num5_width = 0;
 let num6_width = 0;
 
+var count = 0;
+
 const hoursRemaining = document.querySelector("#hours_remaining");
 
 function type_bar(target) {
     var value = parseInt(document.getElementById(target).value);
-    console.log(value);
-    if (value > 24 || value < 0 || time_remaining - value < 0) {
+    if (value < 0 || value > 24 || time_remaining - value < 0 || time_remaining - value > 24) {
         value = 0;
     }
-    if (value >= 0 && value <= 24 && time_remaining - value >= 0) {
+    if ((value >= 0 && value <= 24) && (time_remaining - value >= 0 && time_remaining - value <= 24)) {
         if (target == "num1") {
             num1_width = value * 38;
         }
@@ -44,7 +45,10 @@ function type_bar(target) {
 
 function increment(target) {
     var value = parseInt(document.getElementById(target).value);
-    if (value < 24 && time_remaining > 0) {
+    if (value < 0) {
+        document.getElementById(target).value = value + 1;
+    }
+    else if (value < 24 && time_remaining > 0) {
         value += 1;
         time_remaining -= 1;
         updateBarUp(target);
@@ -56,7 +60,51 @@ function increment(target) {
 
 function decrement(target) {
     var value = parseInt(document.getElementById(target).value);
-    if (value > 0 && time_remaining < 24) {
+    if (value > 0 && value <= 24 && (value <= time_remaining)) {
+        value -= 1;
+        time_remaining += 1;
+        updateBarDown(target);
+        health_width += 38;
+        updateRemaining();
+        document.getElementById(target).value = value;
+        if (value = time_remaining) {
+            count = 0;
+        }
+    }
+    if (count == 0) {
+        if (value > time_remaining + 1) {
+            document.getElementById(target).value = value - 1;
+        }
+        else if (value == time_remaining + 1) {
+            value -= 1;
+            document.getElementById(target).value = value;
+            document.getElementById(target + "_bar").style.width = value * 38 + "px";
+            if (target == "num1") {
+                num1_width = value * 38;
+            }
+            else if (target == "num2") {
+                num2_width = value * 38;
+            }
+            else if (target == "num3") {
+                num3_width = value * 38;
+            }
+            else if (target == "num4") {
+                num4_width = value * 38;
+            }
+            else if (target == "num5") {
+                num5_width = value * 38;
+            }
+            else if (target == "num6") {
+                num6_width = value * 38;
+            }
+            health_width = 0;
+            time_remaining = 0;
+            updateRemaining();
+            document.getElementById("health_bar").style.width = "0px";
+            count += 1;
+        }
+    }
+    else if (value > time_remaining) {
         value -= 1;
         time_remaining += 1;
         updateBarDown(target);
